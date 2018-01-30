c4a49cd91444fbbd7421604e1eb5ee7c615ba153
josduj <josduj@users.noreply.github.com>
2018-01-30 20:06:45 +0100

    add support for token authorization

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ module.exports = (agenda, options) => {
   if (!options.middleware) {
     options.middleware = 'express';
   }
+  if (!options.apiMiddleware) {
+    options.apiMiddleware = [];
+  }
 
   const agendash = require('./lib/agendash')(agenda, options);
 
   try {
     const middlewarePath = path.join(__dirname, 'lib/middlewares', options.middleware);
-    return require(middlewarePath)(agendash);
+    return require(middlewarePath)(agendash, options);
   } catch (err) {
     throw new Error('No middleware available for ' + options.middleware);
   }
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,21 @@
-/* global $, _, Backbone */
+/* global $, _, Backbone, window, document */
 $(function () {
+
+  var tokenKey = getCookieValue('token-key')
+
+  if (tokenKey) {
+    $.ajaxSetup({
+      headers: {
+        'Authorization': window.localStorage.getItem(tokenKey)
+      },
+      error: function (x) {
+        if (x.status == 403 || x.status == 401) {
+          window.location.replace('/')
+        }
+      }
+    })
+  }
+
   var CurrentRequestModel = Backbone.Model.extend({
     defaults: {
       refreshInterval: 2000,
@@ -411,4 +427,10 @@ $(function () {
       dataType: 'json'
     })
   }
+
+  function getCookieValue(a) {
+    var b = document.cookie.match('(^|;)\\s*' + a + '\\s*=\\s*([^;]+)')
+    return b ? b.pop() : ''
+  }
+
 })
