989435955afccf2769c031776108b664a35f1ea5
0215story <byeonghan.baek@gmail.com>
2021-03-22 12:38:32 +0900

    Add json validator message when createJob (#169)
    * Add json validator message when createJob
    
    * feat(server): convert to vuejs data handling from jQuery
    
    * feat(server): convert to vuejs modal display handling from jQuery method
    
    Co-authored-by: Josh Baek <josh@daangn.com>

--- public/app/js/newJob.js
+++ public/app/js/newJob.js
@@ -1,5 +1,6 @@
 const newJob = Vue.component("new-job", {
   data: () => ({
+    jobDataParseError: "",
     jobName: "",
     jobSchedule: "",
     jobRepeatEvery: "",
@@ -8,18 +9,28 @@ const newJob = Vue.component("new-job", {
   props: ["job"],
   methods: {
     clear() {
-      (this.jobName = ""),
+      (this.jobDataParseError = ""),
+        (this.jobName = ""),
         (this.jobSchedule = ""),
         (this.jobRepeatEvery = ""),
-        (this.jobData = "");
+        (this.jobData = `{ "name": "Your medatada goes here..." }`);
     },
     create() {
       const url = `api/jobs/create`;
+
+      let jobData = "";
+      try {
+        jobData = JSON.parse(this.jobData);
+      } catch (err) {
+        this.jobDataParseError = err.message;
+        return;
+      }
+
       let body = {
         jobName: this.jobName,
         jobSchedule: this.jobSchedule,
         jobRepeatEvery: this.jobRepeatEvery,
-        jobData: JSON.parse(this.jobData),
+        jobData: jobData,
       };
       return axios
         .post(url, body)
@@ -27,6 +38,7 @@ const newJob = Vue.component("new-job", {
         .then((data) => {
           this.$emit("popup-message");
           this.$emit("refresh-data");
+          this.$refs.Close.click();
           this.clear();
         })
         .catch(console.log);
@@ -62,11 +74,12 @@ const newJob = Vue.component("new-job", {
               <div class="form-group">
                 <label for="jobData">Job Metadata</label>
                 <prism-editor class="json-editor" :lineNumbers="true" v-model="jobData" language="json"></prism-editor>
+                <small class="form-text text-muted">{{jobDataParseError}}</small>
               </div>
             </form>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-info" data-dismiss="modal" @click="create()">Create Job</button>
+          <button type="button" class="btn btn-info" @click="create()">Create Job</button>
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
         </div>
       </div>
