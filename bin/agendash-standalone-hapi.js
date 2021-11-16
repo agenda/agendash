da24bd7779923ca0e953c5dbda31b0e1ac2f9db4
ali-sharif <ali@movesfinancial.com>
2021-11-16 14:19:29 -0500

    Centralize config loading in startup scripts

--- bin/agendash-standalone-hapi.js
+++ bin/agendash-standalone-hapi.js
@@ -3,39 +3,11 @@
 const { Agenda } = require("agenda");
 const agendash = require("../app");
 const Hapi = require("@hapi/hapi");
-const program = require("commander");
-
-program
-  .option(
-    "-d, --db <db>",
-    "[required] Mongo connection string, same as Agenda connection string"
-  )
-  .option(
-    "-c, --collection <collection>",
-    "[optional] Mongo collection, same as Agenda collection name, default agendaJobs",
-    "agendaJobs"
-  )
-  .option(
-    "-p, --port <port>",
-    "[optional] Server port, default 3000",
-    (n, d) => Number(n) || d,
-    3000
-  )
-  .option(
-    "-t, --title <title>",
-    "[optional] Page title, default Agendash",
-    "Agendash"
-  )
-  .parse(process.argv);
-
-if (!program.db) {
-  console.error("--db required");
-  process.exit(1);
-}
+const program = require("./agendash-options");
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3002,
+    port: program.port,
     host: "localhost",
   });
 
--- bin/agendash-standalone-koa.js
+++ bin/agendash-standalone-koa.js
@@ -3,35 +3,7 @@
 const { Agenda } = require("agenda");
 const agendash = require("../app");
 const Koa = require("koa");
-const program = require("commander");
-
-program
-  .option(
-    "-d, --db <db>",
-    "[required] Mongo connection string, same as Agenda connection string"
-  )
-  .option(
-    "-c, --collection <collection>",
-    "[optional] Mongo collection, same as Agenda collection name, default agendaJobs",
-    "agendaJobs"
-  )
-  .option(
-    "-p, --port <port>",
-    "[optional] Server port, default 3000",
-    (n, d) => Number(n) || d,
-    3000
-  )
-  .option(
-    "-t, --title <title>",
-    "[optional] Page title, default Agendash",
-    "Agendash"
-  )
-  .parse(process.argv);
-
-if (!program.db) {
-  console.error("--db required");
-  process.exit(1);
-}
+const program = require("./agendash-options");
 
 const init = async () => {
   const agenda = new Agenda().database(program.db, program.collection);
