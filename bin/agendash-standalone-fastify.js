816993efed608ed9ca80b21186600cf468148e5f
Vasyl Boroviak <kore.sar@gmail.com>
2022-01-31 15:25:41 +1100

    Fastify support (#201)
    * feat: Add Fastify support (#194)
    
    * Create Fastify middleware
    
    * Add fastify standalone script
    
    * add fastify middleware tests
    
    * Add Fastify middleware documentation
    
    * Fix Vue template...
    
    * fix ObjectId
    
    * move fastify to dev dependencies
    
    Co-authored-by: Vasyl Boroviak <kore.sar@gmail.com>
    
    * Fix after merge conflicts
    
    * Run prettier for all JS files
    
    Co-authored-by: Maxime Chabert <sin2.chabert.maxime@gmail.com>

---
+++ bin/agendash-standalone-fastify.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+"use strict";
+const Agenda = require("agenda");
+const agendash = require("../app");
+const Fastify = require("fastify");
+const program = require("commander");
+
+program
+  .option(
+    "-d, --db <db>",
+    "[required] Mongo connection string, same as Agenda connection string"
+  )
+  .option(
+    "-c, --collection <collection>",
+    "[optional] Mongo collection, same as Agenda collection name, default agendaJobs",
+    "agendaJobs"
+  )
+  .option(
+    "-p, --port <port>",
+    "[optional] Server port, default 3000",
+    (n, d) => Number(n) || d,
+    3000
+  )
+  .option(
+    "-t, --title <title>",
+    "[optional] Page title, default Agendash",
+    "Agendash"
+  )
+  .option(
+    "-p, --path <path>",
+    "[optional] Path to bind Agendash to, default /",
+    "/"
+  )
+  .parse(process.argv);
+
+if (!program.db) {
+  console.error("--db required");
+  process.exit(1);
+}
+
+if (!program.path.startsWith("/")) {
+  console.error("--path must begin with /");
+  process.exit(1);
+}
+
+const fastify = Fastify();
+
+const agenda = new Agenda().database(program.db, program.collection);
+
+fastify.register(
+  agendash(agenda, {
+    middleware: "fastify",
+    title: program.title,
+  })
+);
+
+fastify.listen(program.port, function () {
+  console.log(
+    `Agendash started http://localhost:${program.port}${program.path}`
+  );
+});
